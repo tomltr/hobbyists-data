016ae7cc8b6d28aaf41b46a25f9fb0c1c9f9002c
Tom <49661849+tomltr@users.noreply.github.com>
2019-09-23 09:32:57 -0700

    Add addForm and createNewHobbyist to Controller

--- src/com/controller/HobbyistController.java
+++ src/com/controller/HobbyistController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.entity.Hobbyist;
@@ -30,5 +32,22 @@ public class HobbyistController
 		model.addAttribute("hobbyists", theHobbyists);
 
 		return "list-hobbyists";
+	}
+	
+	@GetMapping("/addForm")
+	public String addForm(Model model)
+	{
+		Hobbyist newHobbyist = new Hobbyist();
+		model.addAttribute("hobbyist", newHobbyist);
+		
+		return "hobbyist-form";
+	}
+	
+	@PostMapping("/createNewHobbyist")
+	public String createNewHobbyist(@ModelAttribute("hobbyist") Hobbyist newHobbyist)
+	{
+		hobbyistService.createHobbyist(newHobbyist);
+		
+		return "redirect:/hobbyist/list";
 	}
 }
