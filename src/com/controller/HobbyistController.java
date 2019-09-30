cd8541df36b9f4e634252c0861c39a60516c4fe4
Tom <49661849+tomltr@users.noreply.github.com>
2019-09-30 09:12:07 -0700

    Refactor HobbyistController

--- src/com/controller/HobbyistController.java
+++ src/com/controller/HobbyistController.java
@@ -31,34 +31,32 @@ public class HobbyistController
 		
 		// add the hobbyists to the model
 		model.addAttribute("hobbyists", theHobbyists);
-
 		return "list-hobbyists";
 	}
 	
 	@GetMapping("/sort")
 	public String sortByOption(@RequestParam("option") String option, Model model)
 	{
-		System.out.println("Option: " + option);
+		// Get new hobbyists list to replace the hobbyists model attribute.
 		List<Hobbyist> theHobbyists = hobbyistService.getHobbyistsByOption(option);
 		model.addAttribute("hobbyists", theHobbyists);
-		
 		return "list-hobbyists";
 	}
 	
 	@GetMapping("/addForm")
 	public String addForm(Model model)
 	{
+		// create new hobbyist and add it to model attribute
 		Hobbyist newHobbyist = new Hobbyist();
 		model.addAttribute("hobbyist", newHobbyist);
-		
 		return "hobbyist-form";
 	}
 	
 	@PostMapping("/saveHobbyist")
 	public String saveHobbyist(@ModelAttribute("hobbyist") Hobbyist newHobbyist)
 	{
+		// insert new hobbyist using hobbyistService
 		hobbyistService.saveHobbyist(newHobbyist);
-		
 		return "redirect:/hobbyists/list";
 	}
 	
@@ -70,26 +68,23 @@ public class HobbyistController
 		
 		// set hobbyist as a model attribute 
 		model.addAttribute("hobbyist", hobbyist);
-		
+
 		// send the attribute info to populate form
-		
 		return "hobbyist-form";
 	}
 	
 	@GetMapping("/remove")
 	public String removeHobbyist(@RequestParam("hobbyistId") int id)
 	{
+		// remove hobbyist according to parameter hobbyistId
 		hobbyistService.removeHobbyist(id);
-		
 		return "redirect:/hobbyists/list";
-		
 	}
 	
 	@GetMapping("/search")
 	public String search(@RequestParam("keyword") String keyword, Model model)
 	{
-		
-		
+		// get new hobbyists list according to parameter keyword
 		List<Hobbyist> resultHobbyists = hobbyistService.search(keyword);
 		model.addAttribute("hobbyists", resultHobbyists);
 		return "list-hobbyists";
